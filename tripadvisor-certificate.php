f358eac5ed28d264ad34b524571fe556a7e10eec
James Warner <james@62design.co.uk>
2015-01-28 14:42:37 +0000

    First commit

---
+++ tripadvisor-certificate.php
@@ -0,0 +1,17 @@
+<?php
+
+use \Blockify\Block;
+use \Blockify\Element;
+
+$block->openTag('div');
+?>
+<div id="TA_certificateOfExcellence440" class="TA_certificateOfExcellence">
+<ul id="6xz143" class="TA_links zhsUXuZmYTo">
+<li id="rk1tLLrymp" class="8MzhoY">
+<a target="_blank" href="http://www.tripadvisor.co.uk/"><img src="//www.tripadvisor.co.uk/img/cdsi/img2/awards/CoE2014_WidgetAsset-14348-2.png" alt="TripAdvisor" class="widCOEImg" id="CDSWIDCOELOGO"/></a>
+</li>
+</ul>
+</div>
+<script src="//www.jscache.com/wejs?wtype=certificateOfExcellence&amp;uniq=440&amp;locationId=<?php echo $block->model['locationId'];?>&amp;lang=<?php echo $block->model['lang'];?>&amp;year=<?php echo $block->model['year'];?>&amp;display_version=2"></script>
+<?php
+$block->closeTag();
